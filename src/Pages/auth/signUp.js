7d87718979dba98e67f07e35260fd66728cda81b
Yuki <beezayrae@gmail.com>
2023-10-01 17:59:02 +0545

    signup initate

--- src/Pages/auth/signUp.js
+++ src/Pages/auth/signUp.js
@@ -1,17 +1,65 @@
-import { Box, FormGroup, TextField } from "@mui/material";
-
-export function SignUp(){
-    return(
+import { Box, FormControl, FormGroup, TextField, Typography } from "@mui/material";
+import img from '../../assests/img/signup.png'
+import { SInputField } from "../../Components/styles/Styles";
+export function SignUp() {
+    return (
         <>
-        <Box container>
-            <form>
-                <FormGroup>
-                    <TextField name="FullName">
+            <Box className="bg-white flex flex-row">
+                <div className="flex-1">
+                <Typography variant="h4" className="text-center">Library Management System</Typography>
 
-                    </TextField>
-                </FormGroup>
-            </form>
-        </Box>
+                    <img src={img} alt="Avatar"  />
+                </div>
+                <div className="flex-1 border-solid border-4 p-8">
+                   
+                    <form>
+                    <div className="header m-4">
+                        <Typography variant="h4" className="text-center">Sign Up</Typography>
+                    </div>
+                    <div className="form-body">
+                        <FormGroup  sx={{ display: `flex`, flexDirection: `row` }}>
+                            <SInputField>
+                                <FormControl>
+                                    <TextField
+                                        size="small"
+                                        required
+                                        label="Name"
+                                    />
+                                </FormControl>
+                            </SInputField>
+                            <SInputField>
+                                <FormControl>
+                                    <TextField 
+                                        size="small"
+                                        required
+                                        label="Name"
+                                    />
+                                </FormControl>
+                            </SInputField>
+                            <SInputField>
+                                <FormControl>
+                                    <TextField
+                                        size="small"
+                                        required
+                                        label="Name"
+                                    />
+                                </FormControl>
+                            </SInputField>
+                            <SInputField>
+                                <FormControl>
+                                    <TextField
+                                         size="small"
+                                        required
+                                        label="Name"
+                                    />
+                                </FormControl>
+                            </SInputField>
+                        </FormGroup>
+                        </div>
+                    </form>
+                </div>
+
+            </Box>
         </>
     )
 }
