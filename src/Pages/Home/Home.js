315c5a44ff44b8fbd4499ad20d09db69ebd30fed
Yuki <beezayrae@gmail.com>
2023-10-04 15:35:32 +0545

    Login SignUp design Complete

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -27,11 +27,11 @@ export default function Home() {
                         setDonutData({
                             ...donutData,
                             labels: response.data.data.preferenceCountList.map((item) => item?.name),
-                            datasets:[{
+                            datasets: [{
                                 ...donutData.datasets[0],
                                 data: response.data.data.preferenceCountList.map((item) => item?.count),
-                                
-                                backgroundColor:randomColor({count:response.data.data.preferenceCountList.length})
+
+                                backgroundColor: randomColor({ count: response.data.data.preferenceCountList.length })
 
                             }],
 
@@ -40,10 +40,10 @@ export default function Home() {
                             ...barData,
                             labels: response.data.data.userActivityCountList.map((item) => item?.monthName),
 
-                            datasets:[{
+                            datasets: [{
                                 ...barData.datasets[0],
                                 data: response.data.data.userActivityCountList.map((item) => item?.presentCount),
-                                
+
 
                             }],
 
@@ -92,10 +92,10 @@ export default function Home() {
 
 
     };
-    
+
     //Donut Graph
-    const [donutData,setDonutData] = useState({
-            labels: [],
+    const [donutData, setDonutData] = useState({
+        labels: [],
         datasets: [{
             label: 'User Count',
             data: [],
@@ -122,7 +122,7 @@ export default function Home() {
 
         }
     }
-   
+
 
 
 
@@ -130,77 +130,74 @@ export default function Home() {
         <>
 
             <Grid container  >
-                <Grid item xs={4}>
-                    <div className="flex flex-1 bg-blue-300  m-2 justify-between p-3 rounded-2xl">
-                        <div className="card flex flex-col w-full">
-                            <div className="header flex justify-between">
-                                <div >
-                                    <Typography variant="h4" sx={{ color: 'rgb(108, 115, 127)' }}>Total Book</Typography>
-                                </div>
-                                <div>
-                                    <GiBlackBook size={50}></GiBlackBook>
-
-                                </div>
+                <Grid item xs={12} sm={6} md={4} lg={4} className="flex">
+                    <div className="card p-3 rounded-2xl m-2 bg-blue-300 flex flex-col w-full">
+                        <div className="header flex justify-between">
+                            <div >
+                                <Typography variant="h4" sx={{ color: 'rgb(108, 115, 127)' }}>Total Book</Typography>
                             </div>
+                            <div>
+                                <GiBlackBook size={50}></GiBlackBook>
 
-                            <div className="flex">
-                                <div className="count">
-                                    <Typography fontSize={40}>{apiData.bookCount}</Typography>
-                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex">
+                            <div className="count">
+                                <Typography fontSize={40}>{apiData.bookCount}</Typography>
                             </div>
                         </div>
                     </div>
                 </Grid>
-                <Grid item xs={4}>
-                    <div className="flex flex-1 bg-red-300  m-2 justify-between p-3 rounded-2xl">
-                        <div className="card flex flex-col w-full">
-                            <div className="header flex justify-between">
-                                <div >
-                                    <Typography variant="h4" sx={{ color: 'rgb(108, 115, 127)' }}>Total Issued</Typography>
-                                </div>
-                                <div>
-                                    <AiOutlineSmile size={50}></AiOutlineSmile>
-
-                                </div>
+                <Grid item xs={12} sm={6} md={4} lg={4} className="flex">
+                    <div className="card  bg-red-300 m-2 p-3 rounded-2xl  flex flex-col w-full">
+                        <div className="header flex justify-between">
+                            <div >
+                                <Typography variant="h4" sx={{ color: 'rgb(108, 115, 127)' }}>Total Issued</Typography>
                             </div>
+                            <div>
+                                <AiOutlineSmile size={50}></AiOutlineSmile>
 
-                            <div className="flex">
-                                <div className="count">
-                                    <Typography fontSize={40}>{apiData.issuedCount}</Typography>
-                                </div>
                             </div>
                         </div>
-                    </div>
-                </Grid>
-                <Grid item xs={4}>
-                    <div className="flex flex-1 bg-orange-300  m-2 justify-between p-3 rounded-2xl">
-                        <div className="card flex flex-col w-full">
-                            <div className="header flex justify-between">
-                                <div >
-                                    <Typography variant="h4" sx={{ color: 'rgb(108, 115, 127)' }}>Total Student</Typography>
-                                </div>
-                                <div>
-                                    <PiStudentLight size={50}></PiStudentLight>
 
-                                </div>
-                            </div>
-
-                            <div className="flex">
-                                <div className="count">
-                                    <Typography fontSize={40}>{apiData.studentCount}</Typography>
-                                </div>
+                        <div className="flex">
+                            <div className="count">
+                                <Typography fontSize={40}>{apiData.issuedCount}</Typography>
                             </div>
                         </div>
                     </div>
                 </Grid>
 
-                <Grid item xs={8}>
+                <Grid item xs={12} sm={6} md={4} lg={4} className="flex">
+                    <div className="card  bg-orange-300  m-2 p-3 rounded-2xl  flex flex-col w-full">
+                        <div className="header flex justify-between">
+                            <div >
+                                <Typography variant="h4" sx={{ color: 'rgb(108, 115, 127)' }}>Total Student</Typography>
+                            </div>
+                            <div>
+                                <PiStudentLight size={50}></PiStudentLight>
+
+                            </div>
+                        </div>
+
+                        <div className="flex">
+                            <div className="count">
+                                <Typography fontSize={40}>{apiData.studentCount}</Typography>
+                            </div>
+                        </div>
+                    </div>
+                </Grid>
+
+
+
+                <Grid item xs={8} >
                     <div className="p-5 rounded-2xl bg-white mr-5" >
                         <Typography variant="h6">User's Activity</Typography>
                         <Bar data={barData} options={options}></Bar>
                     </div>
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item xs={4} >
                     <div className="p-3 bg-white rounded-2xl" >
                         <Doughnut data={donutData} options={donutOptions} ></Doughnut>
                     </div>
--- src/Pages/auth/login.js
+++ src/Pages/auth/login.js
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Container, FormControl, FormControlLabel, FormGroup, TextField, Typography } from "@mui/material";
+import { Box, Button, Checkbox, Container, Divider, FormControl, FormControlLabel, FormGroup, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { loginUser } from "../../Services/apiServices/auth/loginService";
@@ -6,7 +6,9 @@ import { useDispatch } from "react-redux";
 import { setUserDetail } from "../../redux/userDetail";
 import { toast } from "react-toastify";
 import { Link, Navigate, useNavigate } from "react-router-dom";
-
+import img from '../../assests/img/asdf.png'
+import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 export default function Login() {
     const { register, handleSubmit, formState: { isSubmitting } } = useForm()
     const [checked, setChecked] = useState(false);
@@ -45,69 +47,91 @@ export default function Login() {
 
 
     return (
-        <Box className="flex justify-center mt-20">
-            <form onSubmit={handleSubmit(onSubmit)} className="bg-white border  border-solid border-slate-400 " >
-                <Box className="form-header p-5 text-center">
-                    <Typography component="h1" variant="h5">
-                        Sign In
-                    </Typography>
-                </Box>
-                <Box className="form-body p-5">
-                    <FormGroup>
-                        <div className="m-3">
-                        <FormControl>
-                            <TextField
-                                autoComplete="username"
-                                fullWidth
-                                id="email"
-                                label="Email"
-                                size="small"
-                                {...register("email")} />
-                        </FormControl>
-                        </div>
-                        <div className="m-3">
-                        <FormControl>
-                            <TextField
-                                fullWidth
-                                autoComplete="current-password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                size="small"
-                                {...register("password")}
-                            />
-                        </FormControl>
-                        </div>
-                        <div className="flex m-3">
-                            <FormControlLabel
-                                sx={{ alignItems: `center` }}
-                                label="Remember Me"
-                                onChange={handleCheckBox}
-                                control={<Checkbox value="remember" />} />
-                            {/* <Button
-                            type="button"
-                            variant="text"
-                            color="primary"
-                            sx={{ textTransform: `none`, outlineColor: `white` }}
-                        > Forgot Password</Button> */}
-                        </div>
-                        <div className="flex justify-between mt-5">
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                sx={{ textTransform: `none` }}
-                            > {isSubmitting ? "Signing in.." : "Sign In"}</Button>
-                            <Link to="/SignUp">
+        <Box className="flex justify-center mt-10 m-auto   bg-white border  w-4/5   shadow-2xl rounded-2xl" >
+            <div className="w-full ">
+
+                <img src={img} style={{ maxHeight: "100%", maxWidth: "100%", margin: "auto", marginTop: "50px" }}></img>
+            </div>
+            <div className="w-full max-h-full">
+                <form onSubmit={handleSubmit(onSubmit)} >
+                    <Box className="form-header p-5 text-center">
+                        <Typography component="h1" variant="h4" className="font-bold">
+                            Sign In
+                        </Typography>
+                    </Box>
+                    <Box className="form-body p-5">
+                        <FormGroup sx={{ alignContent: "space-around" }}>
+                            <div className="m-3 w-2/4">
+                                <FormControl fullWidth>
+                                    <TextField
+                                        fullWidth
+                                        id="email"
+                                        label="Email"
+                                        size="small"
+                                        inputProps={{
+                                            style:{fontSize:"15px"}
+                                        }}
+                                        {...register("email")} />
+                                </FormControl>
+                            </div>
+                            <div className="m-3 w-2/4">
+                                <FormControl fullWidth>
+                                    <TextField
+                                        label="Password"
+                                        type="password"
+                                        inputProps={{
+                                            style:{fontSize:"15px"}
+                                        }}
+                                        id="password"
+                                        size="small"
+                                        {...register("password")}
+                                    />
+                                </FormControl>
+                            </div>
+                            <div className="flex m-3">
+                                <FormControlLabel
+                                    sx={{ alignItems: `center` }}
+                                    label="Remember Me"
+                                    onChange={handleCheckBox}
+                                    control={<Checkbox value="remember" />} />
+                                <Link>
+                                    <Button
+                                        type="button"
+                                        variant="text"
+                                        color="primary"
+                                        sx={{ textTransform: `none`, outlineColor: `white` }}
+                                    > Forgot Password</Button>
+                                </Link>
+
+                            </div>
+                            <div className="flex flex-col  mt-5">
+                                <div>
                                 <Button
-                                    type="button"
-                                    variant="outlined"
+                                    fullWidth
+                                    type="submit"
+                                    variant="contained"
                                     sx={{ textTransform: `none` }}
-                                > Sign Up</Button>
-                            </Link>
-                        </div>
-                    </FormGroup>
-                </Box>
-            </form>
+                                > {isSubmitting ? "Signing in.." : "Sign In"}</Button>
+                                </div>
+                                <div className="mt-5">
+                                <Divider sx={{ fontSize: "15px", color: "rgba(0, 0, 0, 0.12)" }}> Or</Divider>
+                                        <div className="social flex justify-center">
+
+                                            <FaFacebook size={30} color="#4267B2" className="m-2"></FaFacebook>
+                                            <FcGoogle size={30} className="m-2"></FcGoogle>
+                                            <FaTwitter size={30} color="#1DA1F2" className="m-2"></FaTwitter>
+                                        </div>
+                                  
+                                <div className="text-center font-thin font-sans mt-5">
+                                    Not a User? <Link to="/SignUp" className="no-underline text-blue-500 font-bold"> Sign Up Now </Link>
+                            </div>
+                                </div>
+                             
+                            </div>
+                        </FormGroup>
+                    </Box>
+                </form>
+            </div>
         </Box>
 
     )
